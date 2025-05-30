fc667c2739ab95f1bd86a70356de308a5a8ff8b2
Tienchan27 <trandinhtien05@gmail.com>
2025-05-30 23:09:41 +0700

    Update THREADPOOL.h

--- THREADPOOL.h
+++ THREADPOOL.h
@@ -11,58 +11,61 @@
 #include <functional>
 
 class ThreadPool {
-    public:
-        ThreadPool(int numThreads);
-        enqueue 
-        ~ThreadPool();
-
     private:
         std::mutex queueMutex;
         std::vector<std::thread> workerThreads;
         std::queue< std::function<void()> > tasks;
         std::condition_variable condition;
         bool stopTask;
+
+    public:
+
+    ThreadPool(int numThreads)
+    {
+        for (int i = 0; i < numThreads; i++){
+            workerThreads.emplace_back(
+                [this](){
+                    while(true)
+                    {
+                        std::function<void()> task;
+                        
+                        {
+                            std::unique_lock<std::mutex> lock(queueMutex); //Kiểm soát mutex
+                            //Đợi được Thread nào đó gọi dậy -> nếu dừng task queue hoặc còn việc (task queue chưa empty) thì dậy
+                            this->condition.wait(lock, [this](){return this->stopTask || !this->tasks.empty();}); 
+                            //Dừng task queue và hết việc rồi thì thoát khỏi vòng lặp vô tận
+                            if(this->stopTask && this->tasks.empty())
+                                break;
+                            //Lấy task vào cho thread làm
+                            task = std::move(this->tasks.front());
+                            this->tasks.pop();
+                        }
+
+                        //Do the task
+                        task();
+                    }
+                }
+            );
+        }
+    }
+
+    
+
+    ~ThreadPool()
+    {
+        {
+            std::unique_lock<std::mutex> lock(queueMutex);
+            stopTask = true;
+        }
+        condition.notify_all();
+        for (std::thread& worker : this->workerThreads)
+            worker.join()
+    }
+
+    
     
 };
 
-ThreadPool::ThreadPool(int numThreads)
-{
-    for (int i = 0; i < numThreads; i++){
-        workerThreads.emplace_back(
-            [this](){
-                while(true)
-                {
-                    std::function<void()> task;
-                    
-                    {
-                        std::unique_lock<std::mutex> lock(queueMutex); //Kiểm soát mutex vào
-                        //Đợi được Thread nào đó gọi dậy -> nếu dừng task queue hoặc còn việc (task queue chưa empty) thì dậy
-                        this->condition.wait(lock, [this](){return this->stopTask || !this->tasks.empty();}); 
-                        //Dừng task queue và hết việc rồi thì thoát khỏi vòng lặp vô tận
-                        if(this->stopTask && this->tasks.empty())
-                            break;
-                        //Lấy task vào cho thread làm
-                        task = std::move(this->tasks.front());
-                        this->tasks.pop();
-                    }
 
-                    //Do the task
-                    task();
-                }
-            }
-        );
-    }
-}
-
-ThreadPool::~ThreadPool()
-{
-    {
-        std::unique_lock<std::mutex> lock(queueMutex);
-        stopTask = true;
-    }
-    condition.notify_all();
-    for (std::thread& worker : this->workerThreads)
-        worker.join()
-}
 
 #endif
