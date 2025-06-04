02d3ec8604ed2d178a86e9e96c52add9ec7dabc9
Tienchan27 <trandinhtien05@gmail.com>
2025-06-04 15:32:52 +0700

    Demo LWP

---
+++ DemoLightweightProcesses/LWP.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "=========LIGHTWEIGHT PROCESSES DEMOSTRATION=========" << std::endl;
+    std::cout << "------------------------------------------" << std::endl;
+
+    int numThread;
+    std::cout << "Input number of threads in Thread Pool: ";
+    std::cin >> numThread;
+}
